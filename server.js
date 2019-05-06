21574bb23b9dad2924940df392dbf4ad0c9dbb00
Victor Moreno <victorgalves@hotmail.com>
2019-05-06 13:14:54 -0300

    Instala o express

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const http = require('http')
 
-const server = http.createServer((req, resp) => 
-resp.end(`<html><body><a href="http://www.alura.com.br">Site da Alura</a></body></html>`));
+const server = http.createServer((req, resp) => { 
+    let html = '';
+    if(req.url == '/') {
+        html = `<html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+                </html>`
+    } else if(req.url == '/livros') {
+        html = `<html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Listagem de livros </h1>
+                </body> 
+                </html>`
+    }
+    resp.end(html);
+});
 
 console.log('Servidor rodando na porta: 3000');
 server.listen(3000);
