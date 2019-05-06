9688dd51a446bda831105ea0b2ff5906c26caacb
Victor Moreno <victorgalves@hotmail.com>
2019-05-06 13:51:53 -0300

    Cria o servidor utilizando Express

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-const http = require('http')
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, resp) => { 
-    let html = '';
-    if(req.url == '/') {
-        html = `<html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body> 
-                </html>`
-    } else if(req.url == '/livros') {
-        html = `<html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Listagem de livros </h1>
-                </body> 
-                </html>`
-    }
-    resp.end(html);
+app.listen(3000, function() {
+    console.log('Servidor rodando na porta: 3000');
 });
 
-console.log('Servidor rodando na porta: 3000');
-server.listen(3000);
+app.get('/', (req, resp) => {
+    resp.send(
+        `
+            <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body> 
+            </html>
+        `
+    )
+});
+
+app.get('/livros', (req, resp) => {
+    resp.send(
+        `
+            <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body> 
+            </html>
+        `
+    )
+});
